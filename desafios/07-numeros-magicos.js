f4f30683c4df8f0a1ba32f24874bc1aa8a6faa0e
Gabriel Viana <viannadoze@gmail.com>
2024-08-27 15:55:36 -0300

    ✅ 07 Desafio concluido.

--- desafios/07-numeros-magicos.js
+++ desafios/07-numeros-magicos.js
@@ -2,4 +2,4 @@ function lookForUpdades() {}
 
 setInterval(lookForUpdades, 60 * 30 * 1000)
 
-function calculateDiscount(price, discount) {}
+function calculateDiscount(priceInCents, discountInPercentage) {}

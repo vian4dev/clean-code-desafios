d64126ab8658032e81d93b53584427838e15736d
Gabriel Viana <viannadoze@gmail.com>
2024-08-27 14:53:50 -0300

    ✅ 06 Desafio concluido.

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,11 +1,17 @@
-function updateUserRoute(body, params) {
-  updateUserController(body, params)
+function updateUserRoute({ body, params }) {
+  const { name, email, password } = body
+  const { id } = params
+
+  updateUserController({ data: { name, email, password }, params: { id } })
 }
 
-function updateUserController(data, params) {
-  userRepository.update(data, params)
+function updateUserController({ data, params }) {
+  const { name, email, password } = data
+  const { id } = params
+
+  userRepository.update({ data: { name, email, password }, params: { id } })
 }
 
 const userRepository = {
-  update: (data, params) => {},
+  update: ({ data, params }) => {},
 }

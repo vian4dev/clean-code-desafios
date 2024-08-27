76dac230cf5ff8c8a1995ecc26f180de3410cdf3
Gabriel Viana <viannadoze@gmail.com>
2024-08-27 10:39:36 -0300

    ✅ 05 Desafio concluido.

--- desafios/05-regras-em-condicionais.ts
+++ desafios/05-regras-em-condicionais.ts
@@ -1,19 +1,26 @@
 const necessaryGradeToBeApproved = 7
 const studentGrade = 10
 
-const numberOfAbsensesToFailSchool = 100
-const studentNumberOfAbsenses = 109
+const numberOfAbsencesToFailSchool = 100
+const studentNumberOfAbsences = 109
 
 function checkIfStudentPassedTheSchoolYear() {
   if (studentGrade < necessaryGradeToBeApproved) {
-    return { error: true, message: 'Student was not approved because his grade was below the necessary.'}
-  } else {
-    if(studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
-      return { error: true, message: 'Student was not approved because of his absenses'}
-    } else {
-      return { error: false, message: 'Student was approved :)'}
+    return {
+      error: true,
+      message:
+        'Student was not approved because his grade was below the necessary.',
     }
   }
+
+  if (studentNumberOfAbsences > numberOfAbsencesToFailSchool) {
+    return {
+      error: true,
+      message: 'Student was not approved because of his Absences',
+    }
+  }
+
+  return { error: false, message: 'Student was approved :)' }
 }
 
 console.log(checkIfStudentPassedTheSchoolYear())
